1ed9230942d17f1413225a7cdf5c560e22b90b71
Gwendal Roué <gr@pierlis.com>
2010-11-12 12:44:10 +0100

    Remove outdated documentation in GRMustacheTemplate.h

--- Classes/GRMustacheTemplate.h
+++ Classes/GRMustacheTemplate.h
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
 #import "GRMustacheElement.h"
 
 
@@ -36,63 +35,14 @@
 	NSInteger curline;
 	NSMutableArray *elems;
 }
-
-
-// Renders object with the template contained in the templateString argument.
-// Partials would be looked from current working directory.
-+ (NSString *)renderObject:(id)object
-				fromString:(NSString *)templateString
-					 error:(NSError **)outError;
-
-// Renders object with the template loaded from url, whose content must be encoded in UTF8.
-// Partials would be looked relatively to url.
-+ (NSString *)renderObject:(id)object
-		 fromContentsOfURL:(NSURL *)url
-					 error:(NSError **)outError;
-
-// Renders object with the template loaded from bundle, whose content must be encoded in UTF8.
-// Partials would be looked in the bundle.
-+ (NSString *)renderObject:(id)object
-			  fromResource:(NSString *)name
-					bundle:(NSBundle *)bundle
-					 error:(NSError **)outError;
-
-// Renders object with the template loaded from bundle, whose content must be encoded in UTF8.
-// Partials would be looked in the bundle.
-+ (NSString *)renderObject:(id)object
-			  fromResource:(NSString *)name
-			 withExtension:(NSString *)ext
-					bundle:(NSBundle *)bundle
-					 error:(NSError **)outError;
-
-// Returns a template with the templateString argument.
-// Partials would be looked from current working directory.
-+ (id)parseString:(NSString *)templateString
-			error:(NSError **)outError;
-
-// Returns a template loaded from url, whose content must be encoded in UTF8.
-// Partials would be looked relatively to url.
-+ (id)parseContentsOfURL:(NSURL *)url
-				   error:(NSError **)outError;
-
-// Returns a template loaded from bundle, whose content must be encoded in UTF8.
-// Partials would be looked in the bundle.
-+ (id)parseResource:(NSString *)name
-			 bundle:(NSBundle *)bundle
-			  error:(NSError **)outError;
-
-// Returns a template loaded from bundle, whose content must be encoded in UTF8.
-// Partials would be looked in the bundle.
-+ (id)parseResource:(NSString *)name
-	  withExtension:(NSString *)ext
-			 bundle:(NSBundle *)bundle
-			  error:(NSError **)outError;
-
-
-// Renders with a context object
++ (NSString *)renderObject:(id)object fromString:(NSString *)templateString error:(NSError **)outError;
++ (NSString *)renderObject:(id)object fromContentsOfURL:(NSURL *)url error:(NSError **)outError;
++ (NSString *)renderObject:(id)object fromResource:(NSString *)name bundle:(NSBundle *)bundle error:(NSError **)outError;
++ (NSString *)renderObject:(id)object fromResource:(NSString *)name withExtension:(NSString *)ext bundle:(NSBundle *)bundle error:(NSError **)outError;
++ (id)parseString:(NSString *)templateString error:(NSError **)outError;
++ (id)parseContentsOfURL:(NSURL *)url error:(NSError **)outError;
++ (id)parseResource:(NSString *)name bundle:(NSBundle *)bundle error:(NSError **)outError;
++ (id)parseResource:(NSString *)name withExtension:(NSString *)ext bundle:(NSBundle *)bundle error:(NSError **)outError;
 - (NSString *)renderObject:(id)object;
-
-// Renders without context
 - (NSString *)render;
-
 @end
