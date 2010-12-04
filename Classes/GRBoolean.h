51664efa37d6b9714ffe39c5489f823e9a018d73
Gwendal Roué <gr@pierlis.com>
2010-12-04 14:21:16 +0100

    Deprecate GRYes and GRNo

--- Classes/GRBoolean.h
+++ Classes/GRBoolean.h
@@ -39,7 +39,7 @@
  @returns the GRYes singleton.
  @since v1.0.0
  */
-+ (GRYes *)yes;
++ (GRYes *)yes __attribute__((deprecated));
 
 /**
  @returns YES
@@ -67,7 +67,7 @@
  @returns the GRNo singleton.
  @since v1.0.0
  */
-+ (GRNo *)no;
++ (GRNo *)no __attribute__((deprecated));
 
 /**
  @returns NO
