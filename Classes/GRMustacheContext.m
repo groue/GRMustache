f96a1a832b12d95d4e57c70985e8f057d401ad5c
Gwendal Roué <gr@pierlis.com>
2011-03-17 16:31:00 +0100

    Rewritten valueForUndefinedKey: silencing code according to Mike Ash's advice

--- Classes/GRMustacheContext.m
+++ Classes/GRMustacheContext.m
@@ -34,51 +34,152 @@
 #import "GRMustacheLambda_private.h"
 
 #ifdef DEBUG
+//=============================================================================
+// Embedding https://github.com/rentzsch/jrswizzle/ START
+//=============================================================================
+//	Copyright (c) 2007-2011 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
+//	Some rights reserved: http://opensource.org/licenses/mit-license.php
 
-static id silentValueForUndefinedKeyIMP(id self, SEL _cmd, NSString *key) {
-	return nil;
-}
+@interface NSObject (JRSwizzle)
 
-// The purpose of this function is to have the same result as
-// [object valueForKey:key], but instead of letting [NSObject valueForUndefinedKey:]
-// raise an NSUndefinedKeyException, it silently returns nil instead.
-static id silentValueForKey(id object, NSString *key) {
-	// Does object provide the same implementations of valueForKey: and valueForUndefinedKey: as NSObject?
-	// If it does not, don't mess with it.
-	Class originalClass = [object class];
-	SEL vfukSelector = @selector(valueForUndefinedKey:);
-	SEL vfkSelector = @selector(valueForKey:);
-	if ((class_getMethodImplementation([NSObject class], vfukSelector) !=
-		 class_getMethodImplementation(originalClass, vfukSelector))
-		||
-		(class_getMethodImplementation([NSObject class], vfkSelector) !=
-		 class_getMethodImplementation(originalClass, vfkSelector)))
-	{
-		return [object valueForKey:key];
++ (BOOL)jr_swizzleMethod:(SEL)origSel_ withMethod:(SEL)altSel_ error:(NSError**)error_;
++ (BOOL)jr_swizzleClassMethod:(SEL)origSel_ withClassMethod:(SEL)altSel_ error:(NSError**)error_;
+
+@end
+
+#import <objc/objc-class.h>
+
+#define SetNSErrorFor(FUNC, ERROR_VAR, FORMAT,...)	\
+    if (ERROR_VAR) {	\
+        NSString *errStr = [NSString stringWithFormat:@"%s: " FORMAT,FUNC,##__VA_ARGS__]; \
+        *ERROR_VAR = [NSError errorWithDomain:@"NSCocoaErrorDomain" \
+                                         code:-1	\
+                                     userInfo:[NSDictionary dictionaryWithObject:errStr forKey:NSLocalizedDescriptionKey]]; \
+    }
+#define SetNSError(ERROR_VAR, FORMAT,...) SetNSErrorFor(__func__, ERROR_VAR, FORMAT, ##__VA_ARGS__)
+
+#if OBJC_API_VERSION >= 2
+#define GetClass(obj)	object_getClass(obj)
+#else
+#define GetClass(obj)	(obj ? obj->isa : Nil)
+#endif
+
+@implementation NSObject (JRSwizzle)
+
++ (BOOL)jr_swizzleMethod:(SEL)origSel_ withMethod:(SEL)altSel_ error:(NSError**)error_ {
+#if OBJC_API_VERSION >= 2
+	Method origMethod = class_getInstanceMethod(self, origSel_);
+	if (!origMethod) {
+		SetNSError(error_, @"original method %@ not found for class %@", NSStringFromSelector(origSel_), [self className]);
+		return NO;
 	}
 	
-	// Now the magic: let's temporarily switch object's class with a subclass
-	// whose implementation for valueForUndefinedKey: just returns nil.
-	const char *silentClassName = [[NSString stringWithFormat:@"GRMustacheSilent%@", originalClass] UTF8String];
-	Class silentClass = objc_lookUpClass(silentClassName);
-	if (silentClass == nil) {
-		silentClass = objc_allocateClassPair(originalClass, silentClassName, 0);
-		class_addMethod(silentClass, vfukSelector, (IMP)silentValueForUndefinedKeyIMP, "@@:@");
-		objc_registerClassPair(silentClass);
+	Method altMethod = class_getInstanceMethod(self, altSel_);
+	if (!altMethod) {
+		SetNSError(error_, @"alternate method %@ not found for class %@", NSStringFromSelector(altSel_), [self className]);
+		return NO;
 	}
-	object->isa = silentClass;
 	
-	// Silently call valueForKey!!!!
-	@try {
-		id value = [object valueForKey:key];
-		object->isa = originalClass;
-		return value;
+	class_addMethod(self,
+					origSel_,
+					class_getMethodImplementation(self, origSel_),
+					method_getTypeEncoding(origMethod));
+	class_addMethod(self,
+					altSel_,
+					class_getMethodImplementation(self, altSel_),
+					method_getTypeEncoding(altMethod));
+	
+	method_exchangeImplementations(class_getInstanceMethod(self, origSel_), class_getInstanceMethod(self, altSel_));
+	return YES;
+#else
+	//	Scan for non-inherited methods.
+	Method directOriginalMethod = NULL, directAlternateMethod = NULL;
+	
+	void *iterator = NULL;
+	struct objc_method_list *mlist = class_nextMethodList(self, &iterator);
+	while (mlist) {
+		int method_index = 0;
+		for (; method_index < mlist->method_count; method_index++) {
+			if (mlist->method_list[method_index].method_name == origSel_) {
+				assert(!directOriginalMethod);
+				directOriginalMethod = &mlist->method_list[method_index];
+			}
+			if (mlist->method_list[method_index].method_name == altSel_) {
+				assert(!directAlternateMethod);
+				directAlternateMethod = &mlist->method_list[method_index];
+			}
+		}
+		mlist = class_nextMethodList(self, &iterator);
 	}
-	@catch (NSException *exception) {
-		object->isa = originalClass;
-		[exception raise];
+	
+	//	If either method is inherited, copy it up to the target class to make it non-inherited.
+	if (!directOriginalMethod || !directAlternateMethod) {
+		Method inheritedOriginalMethod = NULL, inheritedAlternateMethod = NULL;
+		if (!directOriginalMethod) {
+			inheritedOriginalMethod = class_getInstanceMethod(self, origSel_);
+			if (!inheritedOriginalMethod) {
+				SetNSError(error_, @"original method %@ not found for class %@", NSStringFromSelector(origSel_), [self className]);
+				return NO;
+			}
+		}
+		if (!directAlternateMethod) {
+			inheritedAlternateMethod = class_getInstanceMethod(self, altSel_);
+			if (!inheritedAlternateMethod) {
+				SetNSError(error_, @"alternate method %@ not found for class %@", NSStringFromSelector(altSel_), [self className]);
+				return NO;
+			}
+		}
+		
+		int hoisted_method_count = !directOriginalMethod && !directAlternateMethod ? 2 : 1;
+		struct objc_method_list *hoisted_method_list = malloc(sizeof(struct objc_method_list) + (sizeof(struct objc_method)*(hoisted_method_count-1)));
+        hoisted_method_list->obsolete = NULL;	// soothe valgrind - apparently ObjC runtime accesses this value and it shows as uninitialized in valgrind
+		hoisted_method_list->method_count = hoisted_method_count;
+		Method hoisted_method = hoisted_method_list->method_list;
+		
+		if (!directOriginalMethod) {
+			bcopy(inheritedOriginalMethod, hoisted_method, sizeof(struct objc_method));
+			directOriginalMethod = hoisted_method++;
+		}
+		if (!directAlternateMethod) {
+			bcopy(inheritedAlternateMethod, hoisted_method, sizeof(struct objc_method));
+			directAlternateMethod = hoisted_method;
+		}
+		class_addMethods(self, hoisted_method_list);
 	}
+	
+	//	Swizzle.
+	IMP temp = directOriginalMethod->method_imp;
+	directOriginalMethod->method_imp = directAlternateMethod->method_imp;
+	directAlternateMethod->method_imp = temp;
+	
+	return YES;
+#endif
 }
+
++ (BOOL)jr_swizzleClassMethod:(SEL)origSel_ withClassMethod:(SEL)altSel_ error:(NSError**)error_ {
+	return [GetClass((id)self) jr_swizzleMethod:origSel_ withMethod:altSel_ error:error_];
+}
+
+@end
+//=============================================================================
+// Embedding https://github.com/rentzsch/jrswizzle/ END
+//=============================================================================
+
+static const NSString *GRMustacheSilentObjects = @"GRMustacheSilentObjects";
+
+@interface NSObject(GRMustache)
+- (id)GRMustacheSilentValueForUndefinedKey:(NSString *)key;
+@end
+
+@implementation NSObject(GRMustache)
+- (id)GRMustacheSilentValueForUndefinedKey:(NSString *)key {
+    NSMutableSet *silentObjects = [[[NSThread currentThread] threadDictionary] objectForKey:GRMustacheSilentObjects];
+    if ([silentObjects containsObject:[NSValue valueWithPointer:self]]) {
+        return nil;
+    }
+    return [self GRMustacheSilentValueForUndefinedKey:key];
+}
+@end
 #endif
 
 
@@ -150,6 +251,17 @@ static NSInteger BOOLPropertyType = NSNotFound;
 @implementation GRMustacheContext
 @synthesize object;
 @synthesize parent;
+
+#ifdef DEBUG
++ (void)initialize
+{
+    if (self == [GRMustacheContext class]) {
+        [NSObject jr_swizzleMethod:@selector(valueForUndefinedKey:)
+                        withMethod:@selector(GRMustacheSilentValueForUndefinedKey:)
+                             error:nil];
+    }
+}
+#endif
 
 + (id)contextWithObject:(id)object {
 	if ([object isKindOfClass:[GRMustacheContext class]]) {
@@ -261,9 +373,16 @@ static NSInteger BOOLPropertyType = NSNotFound;
 	
 	@try {
 #ifdef DEBUG
-		value = silentValueForKey(object, key);
-#else
+        NSMutableSet *silentObjects = [[[NSThread currentThread] threadDictionary] objectForKey:GRMustacheSilentObjects];
+        if (silentObjects == nil) {
+            silentObjects = [NSMutableSet set];
+            [[[NSThread currentThread] threadDictionary] setObject:silentObjects forKey:GRMustacheSilentObjects];
+        }
+        [silentObjects addObject:[NSValue valueWithPointer:object]];
+#endif
 		value = [object valueForKey:key];
+#ifdef DEBUG
+        [silentObjects removeObject:object];
 #endif
 	}
 	@catch (NSException *exception) {
