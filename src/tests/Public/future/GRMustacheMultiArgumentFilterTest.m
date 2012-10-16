b32528ce7faac4140329fa47d53405946d3ed516
Gwendal Roué <gr@pierlis.com>
2012-10-16 20:11:04 +0200

    Test multi-arguments filter with nil input (turned into NSNull)

--- src/tests/Public/future/GRMustacheMultiArgumentFilterTest.m
+++ src/tests/Public/future/GRMustacheMultiArgumentFilterTest.m
@@ -6,16 +6,34 @@
 
 @implementation GRMustacheMultiArgumentFilterTest
 
-- (void)testBlahBlock
+- (void)testMultiArgumentsFiltersCanAccessAllArguments
 {
-    GRMustacheFilter *f = [GRMustacheFilter multiArgumentsFilterWithBlock:^id(NSArray *arguments) {
+    GRMustacheFilter *joinFilter = [GRMustacheFilter multiArgumentsFilterWithBlock:^id(NSArray *arguments) {
         return [[arguments valueForKey:@"description"] componentsJoinedByString:@","];
     }];
     
-    id data = @{ @"a": @"a", @"b": @"b" };
-    id filters = @{ @"f": f };
-    NSString *rendering = [GRMustacheTemplate renderObject:data withFilters:filters fromString:@"{{f(a,b)}}" error:NULL];
-    STAssertEqualObjects(rendering, @"a,b", @"");
+    id data = @{ @"a": @"a", @"b": @"b", @"c": @"c" };
+    id filters = @{ @"join": joinFilter };
+    NSString *rendering = [GRMustacheTemplate renderObject:data withFilters:filters fromString:@"{{join(a,b)}} {{join(a,b,c)}}" error:NULL];
+    STAssertEqualObjects(rendering, @"a,b a,b,c", @"");
+}
+
+- (void)testMultiArgumentsFiltersHaveNSNullArgumentForNilInput
+{
+    __block NSUInteger NSNullCount = 0;
+    GRMustacheFilter *filter = [GRMustacheFilter multiArgumentsFilterWithBlock:^id(NSArray *arguments) {
+        for (id argument in arguments) {
+            if (argument == [NSNull null]) {
+                ++NSNullCount;
+            }
+        }
+        return nil;
+    }];
+    
+    id data = @{ };
+    id filters = @{ @"f": filter };
+    [GRMustacheTemplate renderObject:data withFilters:filters fromString:@"{{f(a,b,c)}}" error:NULL];
+    STAssertEquals(NSNullCount, (NSUInteger)3, @"");
 }
 
 @end
