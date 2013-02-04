7d5d859d1a4279a37b70884e9ed5f8be006db7ed
Gwendal Roué <gr@pierlis.com>
2013-02-04 21:29:28 +0100

    `javascript.escape` and `URL.escape` WIP

--- src/classes/GRMustacheJavascriptLibrary.m
+++ src/classes/GRMustacheJavascriptLibrary.m
@@ -32,9 +32,124 @@
 
 - (id)transformedValue:(id)object
 {
-#warning TODO
-    NSAssert(NO, @"TODO");
-    return nil;
+    NSString *string = [object description];
+    
+    NSUInteger length = [string length];
+    if (!length) {
+        return string;
+    }
+    
+    const UniChar *characters = CFStringGetCharactersPtr((CFStringRef)string);
+    if (!characters) {
+        NSMutableData *data = [NSMutableData dataWithLength:length * sizeof(UniChar)];
+        [string getCharacters:[data mutableBytes] range:(NSRange){ .location = 0, .length = length }];
+        characters = [data bytes];
+    }
+    
+    static const NSString *escapeForCharacter[] = {
+        // Those encodings come from https://github.com/django/django/commit/8c4a525871df19163d5bfdf5939eff33b544c2e2#django/template/defaultfilters.py
+        //
+        // Quoting Malcolm Tredinnick:
+        // > Added extra robustness to the escapejs filter so that all invalid
+        // > characters are correctly escaped. This avoids any chance to inject
+        // > raw HTML inside <script> tags. Thanks to Mike Wiacek for the patch
+        // > and Collin Grady for the tests.
+        //
+        // Quoting Mike Wiacek from https://code.djangoproject.com/ticket/7177
+        // > The escapejs filter currently escapes a small subset of characters
+        // > to prevent JavaScript injection. However, the resulting strings can
+        // > still contain valid HTML, leading to XSS vulnerabilities. Using hex
+        // > encoding as opposed to backslash escaping, effectively prevents
+        // > Javascript injection and also helps prevent XSS. Attached is a
+        // > small patch that modifies the _js_escapes tuple to use hex encoding
+        // > on an expanded set of characters.
+        //
+        // The initial django commit used `\xNN` syntax. The \u syntax was
+        // introduced later for JSON compatibility.
+        
+        [0x00] = @"\\u0000",
+        [0x01] = @"\\u0001",
+        [0x02] = @"\\u0002",
+        [0x03] = @"\\u0003",
+        [0x04] = @"\\u0004",
+        [0x05] = @"\\u0005",
+        [0x06] = @"\\u0006",
+        [0x07] = @"\\u0007",
+        [0x08] = @"\\u0008",
+        [0x09] = @"\\u0009",
+        [0x0A] = @"\\u000A",
+        [0x0B] = @"\\u000B",
+        [0x0C] = @"\\u000C",
+        [0x0D] = @"\\u000D",
+        [0x0E] = @"\\u000E",
+        [0x0F] = @"\\u000F",
+        [0x10] = @"\\u0010",
+        [0x11] = @"\\u0011",
+        [0x12] = @"\\u0012",
+        [0x13] = @"\\u0013",
+        [0x14] = @"\\u0014",
+        [0x15] = @"\\u0015",
+        [0x16] = @"\\u0016",
+        [0x17] = @"\\u0017",
+        [0x18] = @"\\u0018",
+        [0x19] = @"\\u0019",
+        [0x1A] = @"\\u001A",
+        [0x1B] = @"\\u001B",
+        [0x1C] = @"\\u001C",
+        [0x1D] = @"\\u001D",
+        [0x1E] = @"\\u001E",
+        [0x1F] = @"\\u001F",
+        ['\\'] = @"\\u005C",
+        ['\''] = @"\\u0027",
+        ['"'] = @"\\u0022",
+        ['>'] = @"\\u003E",
+        ['<'] = @"\\u003C",
+        ['&'] = @"\\u0026",
+        ['='] = @"\\u003D",
+        ['-'] = @"\\u002D",
+        [';'] = @"\\u003B",
+        
+        // 0x2028 and 0x2029 are not included in this table, that would be too
+        // big. See below.
+    };
+    static const int escapeForCharacterLength = sizeof(escapeForCharacter) / sizeof(NSString *);
+    
+    NSMutableString *buffer = nil;
+    const UniChar *unescapedStart = characters;
+    CFIndex unescapedLength = 0;
+    for (NSUInteger i=0; i<length; ++i, ++characters) {
+        const NSString *escape = nil;
+        if (*characters == 0x2028)
+        {
+            escape = @"\\u2028";
+        }
+        else if (*characters == 0x2029)
+        {
+            escape = @"\\u2029";
+        }
+        else
+        {
+            escape = (*characters < escapeForCharacterLength) ? escapeForCharacter[*characters] : nil;
+        }
+        if (escape) {
+            if (!buffer) {
+                buffer = [NSMutableString stringWithCapacity:length];
+            }
+            CFStringAppendCharacters((CFMutableStringRef)buffer, unescapedStart, unescapedLength);
+            CFStringAppend((CFMutableStringRef)buffer, (CFStringRef)escape);
+            unescapedStart = characters+1;
+            unescapedLength = 0;
+        } else {
+            ++unescapedLength;
+        }
+    }
+    if (!buffer) {
+        return string;
+    }
+    if (unescapedLength > 0) {
+        CFStringAppendCharacters((CFMutableStringRef)buffer, unescapedStart, unescapedLength);
+    }
+    return buffer;
 }
 
 @end
--- src/classes/GRMustacheURLLibrary.m
+++ src/classes/GRMustacheURLLibrary.m
@@ -32,9 +32,68 @@
 
 - (id)transformedValue:(id)object
 {
-#warning TODO
-    NSAssert(NO, @"TODO");
-    return nil;
+    NSString *string = [object description];
+    
+    string = [[object description] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+
+    NSUInteger length = [string length];
+    if (!length) {
+        return string;
+    }
+    
+    const UniChar *characters = CFStringGetCharactersPtr((CFStringRef)string);
+    if (!characters) {
+        NSMutableData *data = [NSMutableData dataWithLength:length * sizeof(UniChar)];
+        [string getCharacters:[data mutableBytes] range:(NSRange){ .location = 0, .length = length }];
+        characters = [data bytes];
+    }
+    
+    static const NSString *escapeForCharacter[] = {
+        ['$'] = @"%24",
+        ['&'] = @"%26",
+        ['+'] = @"%2B",
+        [','] = @"%2C",
+        ['/'] = @"%2F",
+        [':'] = @"%3A",
+        [';'] = @"%3B",
+        ['='] = @"%3D",
+        ['?'] = @"%3F",
+        ['@'] = @"%40",
+        [' '] = @"%20",
+        ['\t'] = @"%09",
+        ['#'] = @"%23",
+        ['<'] = @"%3C",
+        ['>'] = @"%3E",
+        ['\"'] = @"%22",
+        ['\n'] = @"%0A",
+        ['\r'] = @"%0D",
+    };
+    static const int escapeForCharacterLength = sizeof(escapeForCharacter) / sizeof(NSString *);
+    
+    NSMutableString *buffer = nil;
+    const UniChar *unescapedStart = characters;
+    CFIndex unescapedLength = 0;
+    for (NSUInteger i=0; i<length; ++i, ++characters) {
+        const NSString *escape = (*characters < escapeForCharacterLength) ? escapeForCharacter[*characters] : nil;
+        if (escape) {
+            if (!buffer) {
+                buffer = [NSMutableString stringWithCapacity:length];
+            }
+            CFStringAppendCharacters((CFMutableStringRef)buffer, unescapedStart, unescapedLength);
+            CFStringAppend((CFMutableStringRef)buffer, (CFStringRef)escape);
+            unescapedStart = characters+1;
+            unescapedLength = 0;
+        } else {
+            ++unescapedLength;
+        }
+    }
+    if (!buffer) {
+        return string;
+    }
+    if (unescapedLength > 0) {
+        CFStringAppendCharacters((CFMutableStringRef)buffer, unescapedStart, unescapedLength);
+    }
+    return buffer;
 }
 
 @end
